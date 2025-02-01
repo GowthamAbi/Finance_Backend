af4ef249e0730265c4ae0b17ddcef7bfef6ea035
GowthamAbi <68263814+GowthamAbi@users.noreply.github.com>
2025-02-01 16:47:52 +0530

    Update expenseRoutes.js

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -4,15 +4,15 @@ const expenseController = require("../controllers/expenseControllers");
 const authMiddleware = require("../middleware/authMiddleware");
 
 // Add Expense
-router.post("/addExpense", authMiddleware, expenseController.addExpense);
+router.post("/", authMiddleware, expenseController.addExpense);
 
 // Get Expenses
-router.get("/getExpenses", authMiddleware, expenseController.getExpenses);
+router.get("/", authMiddleware, expenseController.getExpenses);
 
 // Delete Expense
-router.delete("/deleteExpense/:id", authMiddleware, expenseController.deleteExpense);
+router.delete("/:id", authMiddleware, expenseController.deleteExpense);
 
 // Update Expense
-router.put("/updateExpense/:id", authMiddleware, expenseController.updateExpense);
+router.put("/:id", authMiddleware, expenseController.updateExpense);
 
 module.exports = router;
